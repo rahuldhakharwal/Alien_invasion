ba53a43258117086634b1980196c45045a489fe3
rahuldhakharwal <110666389+rahuldhakharwal@users.noreply.github.com>
2022-12-22 10:49:01 +0530

    Add files via upload

---
+++ program_main.py
@@ -0,0 +1,34 @@
+from base64 import standard_b64encode
+from pstats import Stats
+import sys
+import pygame
+
+from pygame.sprite import Group
+from settings import Settings
+from ship import Ship
+from alien import Alien
+from game_stats import GameStats
+import game_functions as gf 
+
+def run_game():
+    pygame.init()
+    ai_settings=Settings()
+    screen=pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
+    pygame.display.set_caption("ALIEN INVASION")
+    stats=GameStats(ai_settings)
+
+    ship=Ship(ai_settings,screen)
+    bullets=Group()
+    aliens=Group()
+    gf.create_fleet(ai_settings,screen,ship,aliens)
+
+    while True:
+        gf.check_events(ai_settings,screen,ship,bullets)
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings,screen,ship,aliens,bullets)
+            #print(len(bullets))
+            gf.update_aliens(ai_settings,stats,screen,ship,aliens,bullets)
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
+
+run_game()
